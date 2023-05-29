dd098d39fa4bed239dbda54641796f2e62ca60f7
Max Babichevskyi <100165364+MaxBabichevskiy@users.noreply.github.com>
2023-05-29 11:40:18 +0300

    Create project

---
+++ CookBook/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CookBook
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void AddRecipe_Click(object sender, RoutedEventArgs e)
+        {
+            // Логика добавления рецепта
+        }
+
+        private void EditRecipe_Click(object sender, RoutedEventArgs e)
+        {
+            // Логика изменения рецепта
+        }
+
+        private void DeleteRecipe_Click(object sender, RoutedEventArgs e)
+        {
+            // Логика удаления рецепта
+        }
+
+        private void SearchRecipes_Click(object sender, RoutedEventArgs e)
+        {
+            // Логика поиска рецептов
+        }
+    }
+}
