a04ef2b3b46e4c9d3ff9ee28e9b225dbe6d28077
Max Babichevskyi <100165364+MaxBabichevskiy@users.noreply.github.com>
2023-05-30 21:44:20 +0300

    update

--- CookBook/MainWindow.xaml.cs
+++ CookBook/MainWindow.xaml.cs
@@ -1,8 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
+using System.Collections.ObjectModel;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
 using CookBook.Models;
 using CookBook.ViewModels;
 using CookBook.Views;
@@ -19,7 +20,12 @@ namespace CookBook
 
             viewModel = new MainViewModel();
             DataContext = viewModel;
+
+            // Загрузка списка рецептов
+            LoadRecipes();
         }
+
+
 
         private void LoadRecipes()
         {
@@ -46,89 +52,49 @@ namespace CookBook
 
         private void AddRecipe_Click(object sender, RoutedEventArgs e)
         {
-            // Создать экземпляр окна для добавления рецепта
+            // Открываем окно для добавления рецепта
             RecipeWindow recipeWindow = new RecipeWindow();
+            recipeWindow.DataContext = new RecipeViewModel();
+            recipeWindow.ShowDialog();
 
-            // Создать экземпляр ViewModel для окна добавления рецепта
-            RecipeViewModel recipeViewModel = new RecipeViewModel();
-
-            // Установить DataContext окна на созданный ViewModel
-            recipeWindow.DataContext = recipeViewModel;
-
-            // Отобразить окно для добавления рецепта
-            bool? result = recipeWindow.ShowDialog();
-
-            // Если рецепт успешно добавлен (например, по нажатию кнопки "Сохранить")
-            if (result == true)
-            {
-                // Добавить новый рецепт в список
-                viewModel.Recipes.Add(recipeViewModel.Recipe);
-            }
+            // После закрытия окна можно выполнить необходимые действия
+            // Например, обновить список рецептов
+            LoadRecipes();
         }
 
         private void EditRecipe_Click(object sender, RoutedEventArgs e)
         {
-            // Получить выбранный рецепт из списка
-            Recipe selectedRecipe = (Recipe)recipesListBox.SelectedItem;
-
-            if (selectedRecipe != null)
+            // Проверяем, есть ли выбранный рецепт
+            if (viewModel.SelectedRecipe != null)
             {
-                // Создать экземпляр окна для редактирования рецепта
+                // Открываем окно для редактирования рецепта
                 RecipeWindow recipeWindow = new RecipeWindow();
+                recipeWindow.DataContext = new RecipeViewModel(viewModel.SelectedRecipe);
+                recipeWindow.ShowDialog();
 
-                // Создать экземпляр ViewModel для окна редактирования рецепта
-                RecipeViewModel recipeViewModel = new RecipeViewModel(selectedRecipe);
-
-                // Установить DataContext окна на созданный ViewModel
-                recipeWindow.DataContext = recipeViewModel;
-
-                // Отобразить окно для редактирования рецепта
-                bool? result = recipeWindow.ShowDialog();
-
-                // Если рецепт успешно отредактирован (например, по нажатию кнопки "Сохранить")
-                if (result == true)
-                {
-                    // Обновить информацию о рецепте в списке
-                    int index = viewModel.Recipes.IndexOf(selectedRecipe);
-                    viewModel.Recipes[index] = recipeViewModel.Recipe;
-                }
+                // После закрытия окна можно выполнить необходимые действия
+                // Например, обновить список рецептов
+                LoadRecipes();
             }
         }
 
         private void DeleteRecipe_Click(object sender, RoutedEventArgs e)
         {
-            // Получить выбранный рецепт из списка
-            Recipe selectedRecipe = (Recipe)recipesListBox.SelectedItem;
-
-            if (selectedRecipe != null)
+            // Проверяем, есть ли выбранный рецепт
+            if (viewModel.SelectedRecipe != null)
             {
-                // Подтвердить удаление рецепта с помощью диалогового окна
-                MessageBoxResult result = MessageBox.Show("Вы уверены, что хотите удалить рецепт?", "Подтверждение удаления", MessageBoxButton.YesNo);
-
-                if (result == MessageBoxResult.Yes)
-                {
-                    // Удалить рецепт из списка
-                    viewModel.Recipes.Remove(selectedRecipe);
-                }
+                // Логика удаления рецепта
             }
         }
 
         private void SearchRecipes_Click(object sender, RoutedEventArgs e)
         {
-            // Получить текст поиска из текстового поля
-            string searchText = txtSearchName.Text;
+            // Логика поиска рецептов
+        }
 
-            // Выполнить поиск рецептов по тексту
-            List<Recipe> searchResults = viewModel.Recipes.Where(r => r.Name.Contains(searchText)).ToList();
-
-            // Очистить текущий список рецептов
-            viewModel.Recipes.Clear();
-
-            // Добавить результаты поиска в список
-            foreach (Recipe recipe in searchResults)
-            {
-                viewModel.Recipes.Add(recipe);
-            }
+        private void txtSearchName_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            // Логика поиска рецептов по названию
         }
     }
 }
---
+++ CookBook/Models/RecipeRepository.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CookBook.Models
+{
+    public class RecipeRepository
+    {
+        // Метод для получения списка рецептов из базы данных или другого источника данных
+        public List<Recipe> GetRecipes()
+        {
+            // Здесь можно добавить код для получения списка рецептов
+            // из базы данных или другого источника данных
+            // и вернуть его в виде списка объектов Recipe
+
+            // Пример кода для временного заполнения списка рецептов
+            //var recipes = new List<Recipe>
+            //{
+            //    new Recipe { Name = "Рецепт 1", Cuisine = "Кухня 1", Type = "Тип 1", byte[] = "url1", Instructions = "Инструкции 1" },
+            //    new Recipe { Name = "Рецепт 2", Cuisine = "Кухня 2", Type = "Тип 2", ImageUrl = "url2", Instructions = "Инструкции 2" },
+            //    new Recipe { Name = "Рецепт 3", Cuisine = "Кухня 3", Type = "Тип 3", ImageUrl = "url3", Instructions = "Инструкции 3" }
+            //};
+
+            return recipes;
+        }
+    }
+}
