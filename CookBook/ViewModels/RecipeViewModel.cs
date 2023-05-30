a0fc2be4b431ea4cfb3209274567deb2872444f6
Max Babichevskyi <100165364+MaxBabichevskiy@users.noreply.github.com>
2023-05-30 17:42:36 +0300

    write RecipeViewModel

--- CookBook/ViewModels/RecipeViewModel.cs
+++ CookBook/ViewModels/RecipeViewModel.cs
@@ -1,13 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using CookBook.Models;
 using CookBook.ViewModels;
 
 namespace CookBook.ViewModels
 {
-    public class RecipeViewModel
+    public class RecipeViewModel : INotifyPropertyChanged
     {
+        private Recipe _recipe;
+
+        public Recipe Recipe
+        {
+            get { return _recipe; }
+            set
+            {
+                _recipe = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public RecipeViewModel()
+        {
+            Recipe = new Recipe();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
