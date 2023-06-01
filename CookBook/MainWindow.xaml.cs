7c864f7eab317d8e2eabdf3f8da721cc45fb0481
Max Babichevskyi <100165364+MaxBabichevskiy@users.noreply.github.com>
2023-06-01 17:57:06 +0300

    update

--- CookBook/MainWindow.xaml.cs
+++ CookBook/MainWindow.xaml.cs
@@ -28,7 +28,7 @@ namespace CookBook
 
 
 
-        private async void LoadRecipes() // Изменяем метод на асинхронный
+        private async void LoadRecipes()
         {
             try
             {
@@ -37,7 +37,6 @@ namespace CookBook
 
                 // Получить список рецептов из базы данных или другого источника данных
                 List<Recipe> recipes = await recipeRepository.GetRecipesAsync();
-                // Используем асинхронный метод
 
                 // Добавить полученные рецепты в список
                 foreach (Recipe recipe in recipes)
@@ -47,10 +46,10 @@ namespace CookBook
             }
             catch (Exception ex)
             {
-                // Обработка исключения при загрузке рецептов
-                // Можно вывести сообщение об ошибке или выполнить другие действия
+                MessageBox.Show($"Error loading recipes: {ex.Message}");
             }
         }
+
 
         private void AddRecipe_Click(object sender, RoutedEventArgs e)
         {
@@ -87,7 +86,17 @@ namespace CookBook
             // Проверяем, есть ли выбранный рецепт
             if (viewModel.SelectedRecipe != null)
             {
-                // Логика удаления рецепта
+                // Показываем диалоговое окно подтверждения удаления
+                MessageBoxResult result = MessageBox.Show("Are you sure you want to delete this recipe?", "Confirmation", MessageBoxButton.YesNo, MessageBoxImage.Question);
+
+                if (result == MessageBoxResult.Yes)
+                {
+                    // Удаляем выбранный рецепт из базы данных
+                    recipeRepository.DeleteRecipe(viewModel.SelectedRecipe);
+
+                    // Обновляем список рецептов
+                    LoadRecipes();
+                }
             }
         }
 
